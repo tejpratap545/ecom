9637b66c90eab640bd8f01e71f086a1439f59d0f
Tejpratap Singh <tejpratapsingh545@gmail.com>
2023-05-07 16:02:36 +0530

    create user after login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import knex from '../database/connection';
 import moment from 'moment';
+import jwt from 'jsonwebtoken';
 
 function generateOTP(): number {
   return Math.floor(100000 + Math.random() * 900000);
@@ -15,13 +16,21 @@ export async function sendOtp(req: Request, res: Response) {
 
     await knex('authotps')
       .insert({ mobile_number, otp, expires_at })
-    res.status(200).json({ message: 'OTP sent' });
+    res.status(200).json({ message: 'OTP sent', data: { otp: otp, mobile_number: mobile_number } });
   } catch (err) {
     console.log(err);
-    
+
     res.status(500).json({ error: 'Server error' });
   }
 }
+
+function generateToken(userId: number): string {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET || 'secret_key', {
+    expiresIn: '1h',
+  });
+}
+
+ 
 
 export async function verifyOtp(req: Request, res: Response) {
   const { mobile_number, otp } = req.body;
@@ -29,7 +38,8 @@ export async function verifyOtp(req: Request, res: Response) {
   try {
     const otpRecord = await knex('authotps')
       .where({ mobile_number })
-      .andWhere('expires_at', '>', new Date())
+      .andWhereRaw('expires_at > NOW()')
+      .orderBy('id', 'desc')
       .first();
 
     if (!otpRecord || otpRecord.otp !== otp) {
@@ -38,7 +48,29 @@ export async function verifyOtp(req: Request, res: Response) {
 
     await knex('authotps').where({ mobile_number }).del();
 
-    res.status(200).json({ message: 'OTP verified' });
+
+
+    const [existingUser] = await knex('users')
+      .select('*')
+      .where('mobile_number', mobile_number);
+
+    let user;
+
+    if (existingUser) {
+      user = existingUser;
+    } else {
+      const [newUserId] = await knex('users')
+        .insert({ mobile_number })
+        .returning('id');
+
+      [user] = await knex('users').select('*').where('id', newUserId);
+
+    }
+
+    const token = generateToken(user.id);
+
+
+    res.status(200).json({ message: 'OTP verified', data: { token: token, user: user } });
   } catch (err) {
     res.status(500).json({ error: 'Server error' });
   }
---
+++ src/migrations/20230506215500_create_users_table.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('first_name').nullable();
+    table.string('email').unique().nullable();
+    table.string('mobile_number').unique().notNullable();
+    table.text('address').nullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('users');
+}
