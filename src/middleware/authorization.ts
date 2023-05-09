4b00679db23be8bf61e1aaa255eda708e0badb38
Tejpratap Singh <tejpratapsingh545@gmail.com>
2023-05-09 18:43:07 +0530

    adminAuthorization

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -1,4 +1,5 @@
 import {   Response, NextFunction } from 'express';
+import knex from 'knex';
 import { ExtendedRequest } from '../types/extendedRequest';
 
 export function authorization(req: ExtendedRequest, res: Response, next: NextFunction) {
@@ -11,3 +12,27 @@ export function authorization(req: ExtendedRequest, res: Response, next: NextFun
 
   next();
 }
+
+export async function  adminAuthorization(req: ExtendedRequest, res: Response, next: NextFunction) {
+  const userId = req.userId;
+ 
+  if (!userId) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const user = await knex('users')
+    .select('is_admin')
+    .where('id', userId)
+    .first();
+
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  if (!user.is_admin) {
+    return res.status(403).json({ error: 'Access denied. You are not an admin.' });
+  }
+  
+  next();
+}
+
