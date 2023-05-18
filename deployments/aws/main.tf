0d19503cb31c62530234ebbbe261de3a28620d94
Tejpratap Singh <tejpratapsingh545@gmail.com>
2023-05-19 02:27:10 +0530

    fix k8 access isssue

--- deployments/aws/main.tf
+++ deployments/aws/main.tf
@@ -235,10 +235,31 @@ module "eks" {
       min_capacity     = 1
 
       instance_type = "t2.micro"
+      additional_security_group_ids = [aws_security_group.worker_nodes.id]
     }
   }
 }
 
+
+resource "aws_security_group" "worker_nodes" {
+  name        = "worker_nodes"
+  description = "Security group for all nodes in the cluster"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
 
