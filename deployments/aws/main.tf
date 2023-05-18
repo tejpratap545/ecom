6344e8b7efce0c4d7ceed0b779cc6bef5a6f39c5
Tejpratap Singh <tejpratapsingh545@gmail.com>
2023-05-18 19:10:06 +0530

    kube

--- deployments/aws/main.tf
+++ deployments/aws/main.tf
@@ -10,6 +10,37 @@ variable "key_name" {
   default = "aws-dev-001"
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+locals {
+  cluster_name = "learnk8s"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+module "eks-kubeconfig" {
+  source  = "hyperbadger/eks-kubeconfig/aws"
+  version = "2.0.0"
+
+  depends_on = [module.eks]
+  cluster_name = module.eks.cluster_name
+}
+
+resource "local_file" "kubeconfig" {
+  content  = module.eks-kubeconfig.kubeconfig
+  filename = "kubeconfig_${local.cluster_name}"
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.77.0"
@@ -22,6 +53,16 @@ module "vpc" {
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
 }
 
 
@@ -86,13 +127,13 @@ resource "aws_eip" "bation_ip" {
 }
 
 resource "aws_instance" "bastion" {
-  ami                    = var.aws_ami
-  instance_type          = "t2.micro"
-  subnet_id              = tolist(module.vpc.public_subnets)[0]
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
-   associate_public_ip_address = aws_eip.bation_ip.address
-  user_data = <<-EOF
+  ami                         = var.aws_ami
+  instance_type               = "t2.micro"
+  subnet_id                   = tolist(module.vpc.public_subnets)[0]
+  key_name                    = var.key_name
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
+  associate_public_ip_address = aws_eip.bation_ip.address
+  user_data                   = <<-EOF
               #!/bin/bash
               sudo apt-get update
               sudo apt-get install redis-tools -y
@@ -170,5 +211,27 @@ output "aws_instance_kafka_2" {
   value = aws_instance.kafka[2].public_ip
 }
 
+
+
+
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "19.14.0"
+  cluster_name    = local.cluster_name
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+  cluster_version = "1.24"
+
+  eks_managed_node_groups = {
+    first = {
+      desired_capacity = 1
+      max_capacity     = 10
+      min_capacity     = 1
+
+      instance_type = "t2.micro"
+    }
+  }
+}
 
 
